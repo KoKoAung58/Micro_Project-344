51461d3ed8827035234f7e3d94b8dbbdf5a1259a
KoKo Aung <56900429+KoKoAung58@users.noreply.github.com>
2020-09-04 20:39:20 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 
+
 int main() {
     FILE *fptr;
 
     char arr[3][100]; //array
 
-    fptr=fopen("/Users/kka/CLionProjects/Micro_project/cmake-build-debug/Input.txt","r");
+    fptr = fopen("/Users/kka/Desktop/Text.txt", "r");//read the input file
 
     int x = 0;
-    while(x<=3){
-        fgets(arr[x],100,(FILE*) fptr);
+    while (x <= 3) {
+        fgets(arr[x], 100, fptr);
+
         x++;
     }
-    int again=0;
-
-    while(again==0){
-        int Line_Number;
+    int i = 0;
+    int Line_Number;
+    printf("Type any line numbers between 0 to 3\n");
+    while (i == 0) {
         printf("Enter Line Number: ");
+        scanf("%d", &Line_Number);
 
-        scanf("%d",&Line_Number);
-        printf("%s\n", arr[Line_Number]);
-        printf("Type 0 if you want to try it again or type 1 if you want to stop!\n");
-        scanf("%d",&again);
+        if (Line_Number <= 3) {
+            i = 0;
+            printf("%s\n", arr[Line_Number]);
+        } else {
+            i++;
+            printf("Sorry, wrong input.");
+        }
     }
-//    if(Line_Number>3){
-//        printf("Wrong Input");
-//    }
+
     fclose(fptr);
     return 0;
 }
